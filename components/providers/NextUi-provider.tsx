64b5728f89bdc2b03c1f3e5238de558e1adfbbb0
fardeen awais <fardeenjutt99@gmail.com>
2024-03-13 23:24:12 +0500

    Clean the previous version

--- components/providers/NextUi-provider.tsx
+++
@@ -1,8 +0,0 @@
-"use client"
-
-import * as React from "react"
-import {NextUIProvider} from "@nextui-org/react";
-
-export function NextUIProviders({ children}: any) {
-  return <NextUIProvider>{children}</NextUIProvider>
-}
