08f1e4e19ba074f345ac819fa5f90f8edcbc7f09
fardeen awais <fardeenjutt99@gmail.com>
2024-03-13 23:27:00 +0500

    Clean all previous website

--- app/(meeting)/layout.tsx
+++
@@ -1,9 +0,0 @@
-const MainLayout = async ({ children }: { children: React.ReactNode }) => {
-    return (
-        <main>
-            {children}
-        </main>
-    )
-}
-
-export default MainLayout
